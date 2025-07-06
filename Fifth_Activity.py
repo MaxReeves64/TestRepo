9102be030a2b1fb4acc681b35d1fc3953761d524
Max Reeves <maxreeves@Maxs-MBP.attlocal.net>
2025-07-05 17:15:04 -0700

    Added median function to median_branch

--- Fifth_Activity.py
+++ Fifth_Activity.py
@@ -1,1 +1,19 @@
 print("hello_world")
+
+import math
+
+def median(input):
+  input.sort()
+  L = len(input)
+  M = L // 2
+  if L % 2 == 0:
+
+      return (input[M-1] + input[M])/2
+
+  else:
+
+      return input[M]
+
+my_list = [0, 1, 4, 50]
+
+print(median(my_list))
